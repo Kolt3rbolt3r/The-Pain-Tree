58a83e803116d87a3206ac905bb79c42386543fc
Kolt3rbolt3r <160914570+Kolt3rbolt3r@users.noreply.github.com>
2026-08-16 14:43:43 -0500

    Remove gain modification for challenge 13
    Removed cubic root gain modification for challenge 13.

--- js/mod.js
+++ js/mod.js
@@ -53,7 +53,6 @@ function getPointGen() {
 
 	if (inChallenge('p', 11)) gain = gain.sqrt()
 	if (inChallenge('p', 12)) gain = gain.cbrt()
-	if (inChallenge('p', 13)) gain = gain.cbrt()
 
 	return gain
 }
