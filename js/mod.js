fef37864dc3a7a6851c3e55b18c940735a5e2c9e
Kolt3rbolt3r <160914570+Kolt3rbolt3r@users.noreply.github.com>
2026-08-16 14:36:54 -0500

    Add cubic root gain adjustment for challenge 13

--- js/mod.js
+++ js/mod.js
@@ -53,6 +53,7 @@ function getPointGen() {
 
 	if (inChallenge('p', 11)) gain = gain.sqrt()
 	if (inChallenge('p', 12)) gain = gain.cbrt()
+	if (inChallenge('p', 13)) gain = gain.cbrt()
 
 	return gain
 }
