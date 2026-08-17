ccd330d524ec76e4162b4cc63bd9ec76214ef119
Kolt3rbolt3r <160914570+Kolt3rbolt3r@users.noreply.github.com>
2026-08-16 19:44:35 -0500

    Modify layers and add new 'Unlocks' layer

--- js/layers.js
+++ js/layers.js
@@ -1,7 +1,7 @@
 addLayer("p", {
     name: "Particles", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
@@ -20,7 +20,7 @@ addLayer("p", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 1, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
@@ -177,5 +177,42 @@ addLayer("p", {
         if (hasUpgrade('p', 33)) mult = mult.times(upgradeEffect('p', 33))
         return mult
     },
+    },
+ })
+		 
+
+	addLayer("u", {
+    name: "Unlocks",
+    symbol: "U",
+    position: 0,
+    startData() { return {
+        unlocked: true,
+        points: new Decimal(0),
+    }},
+    color: "#33944d",
+    requires: new Decimal(2.5e12),
+    resource: "Unlocks",
+    baseResource: "Atoms",
+    baseAmount() {return player.points},
+    type: "normal",
+    exponent: 0.5,
+    gainMult() {
+        let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() {
+        return new Decimal(1)
+    },
+    row: 1,
+    hotkeys: [
+        {key: "u", description: "U: Reset for unlock points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    upgrades: {
+        11: {
+            title: "More Upgrades",
+            description: "Unlocks new upgrades",
+            cost: new Decimal("2.5e12"),
+        },
     },
 })
