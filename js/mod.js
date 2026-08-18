2d32de469fe338a8f4dad45af6541a7c06abb6dd
Kolt3rbolt3r <160914570+Kolt3rbolt3r@users.noreply.github.com>
2026-08-17 20:45:49 -0500

    Update challenge condition for gain calculation

--- js/mod.js
+++ js/mod.js
@@ -54,7 +54,7 @@ function getPointGen() {
 
 	if (inChallenge('p', 11)) gain = gain.sqrt()
 	if (inChallenge('p', 12)) gain = gain.cbrt()
-	if (inChallenge('p', 13)) gain = gain.root(4)
+	if (inChallenge('p', 21)) gain = gain.root(4)
 
 	return gain
 }
